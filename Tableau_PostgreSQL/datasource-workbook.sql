f0567a1af142cf9dca281ea858de5bb58835246f
Viggneshwar Arunachalam <viggneshwar@gmail.com>
2025-12-28 13:56:33 +0530

    Refactor SQL queries for data sources and workbooks

--- Tableau_PostgreSQL/datasource-workbook.sql
+++ Tableau_PostgreSQL/datasource-workbook.sql
@@ -1,34 +1,57 @@
-/*
-	
-	What data sources are used? 
+--Workbooks with Connected Data Sources
+SELECT 
+    w.id AS workbook_id,
+    w.name AS workbook_name,
+    u.name AS workbook_owner,
+    p.name AS project_name,
+    ds.id AS datasource_id,
+    ds.name AS datasource_name,
+    du.name AS datasource_owner,
+    w.created_at AS workbook_created,
+    w.updated_at AS workbook_updated,
+    ds.created_at AS datasource_created,
+    ds.updated_at AS datasource_updated
+FROM workbooks w
+JOIN workbook_datasources wds ON w.id = wds.workbook_id
+JOIN datasources ds ON wds.datasource_id = ds.id
+JOIN users u ON w.owner_id = u.id
+JOIN users du ON ds.owner_id = du.id
+JOIN projects p ON w.project_id = p.id
+ORDER BY w.name, ds.name;
 
-	Shows published and non-published data sources from Excel workbooks to database connections. 
-*/
+--Count of Data Sources per Workbook
+SELECT 
+    w.name AS workbook_name,
+    COUNT(wds.datasource_id) AS datasource_count
+FROM workbooks w
+JOIN workbook_datasources wds ON w.id = wds.workbook_id
+GROUP BY w.name
+ORDER BY datasource_count DESC;
 
-SELECT
-	ds.is_certified AS "Certified Datasource", 
-	ds.name AS "Tableau Datasource Name", 
-	dc.dbname AS "Datasource Name", 
-	dc.server AS "Datasource Server", 
-	ds.db_class AS "Data Connection Type", 
-	dc.username AS "User Name", 
-	dc.owner_type AS "Workbook or Datasource",
-	CASE 
-		WHEN dc.owner_type = 'Datasource' THEN ds.name  
-		ELSE wkb.name 
-	END AS "Workbook or Datasource name"
-FROM data_connections AS dc 
-    INNER JOIN datasources AS ds ON ( dc.datasource_id = ds.id )
-    LEFT OUTER JOIN workbooks AS wkb ON ( wkb.id = dc.owner_id )
- 
-ORDER BY 
-	ds.name ASC 
+--Data Sources by Project (via Workbooks)
 
-	
+SELECT 
+    p.name AS project_name,
+    ds.name AS datasource_name,
+    COUNT(w.id) AS workbook_count
+FROM datasources ds
+JOIN workbook_datasources wds ON ds.id = wds.datasource_id
+JOIN workbooks w ON wds.workbook_id = w.id
+JOIN projects p ON w.project_id = p.id
+GROUP BY p.name, ds.name
+ORDER BY workbook_count DESC;
 
-	
-	
-	
-	
-	
-	
+--Recently Updated Workbooks and Their Data Sources
+SELECT 
+    w.name AS workbook_name,
+    ds.name AS datasource_name,
+    w.updated_at AS workbook_updated,
+    ds.updated_at AS datasource_updated
+FROM workbooks w
+JOIN workbook_datasources wds ON w.id = wds.workbook_id
+JOIN datasources ds ON wds.datasource_id = ds.id
+WHERE w.updated_at >= CURRENT_DATE - INTERVAL '30 days'
+   OR ds.updated_at >= CURRENT_DATE - INTERVAL '30 days'
+ORDER BY w.updated_at DESC, ds.updated_at DESC;
+
+
